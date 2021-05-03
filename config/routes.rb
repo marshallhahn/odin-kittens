e3c487893f8336bea760b9a94c3fa85455f5b38d
Marshall Hahn <m.hahn@calendly.com>
2021-05-03 13:26:57 -0500

    Complete project setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'kittens#index'
+
   resources :kittens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
